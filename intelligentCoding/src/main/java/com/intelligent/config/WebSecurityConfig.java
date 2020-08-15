aa38fd5be915b84a84a7fb8878a9aa183d232764
yangxia605 <yangxia605@gmail.com>
2020-08-15 21:49:31 +0800

    1.allow cors in web security

--- intelligentCoding/src/main/java/com/intelligent/config/WebSecurityConfig.java
+++ intelligentCoding/src/main/java/com/intelligent/config/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/login",
                         "/register",
