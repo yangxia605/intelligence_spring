77acbf561a0fe34211397aa460fcc696efb616bd
zhangyuhui123 <1510150170@qq.com>
2021-11-22 16:16:33 +0800

    -分页接口p1

--- intelligentCoding/src/main/java/com/intelligent/dao/DiscussionDao.java
+++ intelligentCoding/src/main/java/com/intelligent/dao/DiscussionDao.java
@@ -1,7 +1,17 @@
 package com.intelligent.dao;
 
+import com.intelligent.model.Answer;
 import com.intelligent.model.Discussion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DiscussionDao extends JpaRepository<Discussion, Integer> {
+
+    Discussion findByUserIdAndTopicId(int userId, int topicId);
+
+    @Query(nativeQuery = true, value ="select * from discussion where topic_id=?1")
+    List<Discussion> getDisscussionByTopicId(int TopicId);
+
 }
