72bc78a711b21a4bdaa4a92f27db514f80437c2b
succ <01234>
2021-11-22 17:32:28 +0800

    testController

---
+++ intelligentCoding/src/main/java/com/intelligent/controller/ControllerTest.java
@@ -0,0 +1,29 @@
+package com.intelligent.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.intelligent.controller.type.PageRequest;
+import com.intelligent.controller.type.Result;
+import com.intelligent.service.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+@Component
+@RestController
+public class ControllerTest {
+
+    @Autowired
+    private TopicService topicService;
+
+    @RequestMapping(value = "test01", method = RequestMethod.GET)
+    public String test(@RequestParam("id") int id) {
+        return id + "hello wanxia!";
+    }
+
+    @RequestMapping(value = "test02", method = RequestMethod.POST)
+    public PageInfo getKeywords(@RequestParam("keywords") String keywords, @RequestBody PageRequest pageRequest) {
+        Result pageResult = new Result();
+        return topicService.getByKeyword(keywords, pageRequest);
+    }
+
+}
