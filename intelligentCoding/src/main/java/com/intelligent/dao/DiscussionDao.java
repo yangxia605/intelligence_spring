c0e87521834341a410ba7dfb9e4804995125fb1a
zhangyuhui123 <1510150170@qq.com>
2021-11-23 23:11:54 +0800

    -分页接口p1

--- intelligentCoding/src/main/java/com/intelligent/dao/DiscussionDao.java
+++ intelligentCoding/src/main/java/com/intelligent/dao/DiscussionDao.java
@@ -2,6 +2,7 @@ package com.intelligent.dao;
 
 import com.intelligent.model.Answer;
 import com.intelligent.model.Discussion;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,7 +12,22 @@ public interface DiscussionDao extends JpaRepository<Discussion, Integer> {
 
     Discussion findByUserIdAndTopicId(int userId, int topicId);
 
+//    List<Discussion> selectAll();
+
+    // 获取全部讨论
+    @Query(nativeQuery = true, value ="select * from discussion order by id")
+    List<Discussion> getAll();
+
+    //获取指定讨论ID的讨论
+    @Query(nativeQuery = true, value ="select * from discussion where id=?1")
+    Discussion getDisscussionById(int discussionId);
+
+    // 根据题目ID查找讨论
     @Query(nativeQuery = true, value ="select * from discussion where topic_id=?1")
-    List<Discussion> getDisscussionByTopicId(int TopicId);
+    List<Discussion> getDiscussionByTopicId(int topicId);
+
+    // 根据楼主ID获取讨论
+    @Query(nativeQuery = true, value ="select * from discussion where parent_id=?1")
+    List<Discussion> getDiscussionByParentId(int parentId);
 
 }
