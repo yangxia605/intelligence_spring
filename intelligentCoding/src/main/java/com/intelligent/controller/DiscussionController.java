2b6c8a3d77df64f95de078c433c6aa49a554d85f
zhangyuhui123 <1510150170@qq.com>
2021-11-23 23:11:42 +0800

    -分页接口p1

--- intelligentCoding/src/main/java/com/intelligent/controller/DiscussionController.java
+++ intelligentCoding/src/main/java/com/intelligent/controller/DiscussionController.java
@@ -10,12 +10,15 @@ import com.intelligent.model.Users;
 import com.intelligent.service.DiscussionService;
 import com.intelligent.type.TopicWithUserFavorite;
 import com.intelligent.util.UserContext;
+import com.sun.javafx.collections.MappingChange;
+import org.apache.ibatis.jdbc.Null;
 import org.junit.runners.Parameterized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -28,28 +31,41 @@ public class DiscussionController {
 
     // 通过题目ID获取讨论区内容
     @RequestMapping(value = "getDiscussionByTopicId", method = RequestMethod.GET)
-    public List<Discussion> getDiscussionStatus(@RequestParam("topicId") int topicId){
-        List<Discussion> discussionsList = new ArrayList<>();
-        discussionsList = discussionService.getDisscussionByTopicId(topicId);
-        return discussionsList;
+    public Result<Map<Discussion,List<Discussion>>> getDiscussionStatus(@RequestParam("topicId") int topicId){
+        Map<Discussion,List<Discussion>> discussionListMap = new HashMap<>();
+        //List<Discussion> discussionsList = new ArrayList<>();
+        discussionListMap = discussionService.getDiscussionByTopicId(topicId);
+        Result<Map<Discussion,List<Discussion>>> resultDiscussion = new Result<>();
+        resultDiscussion.setSuccess(true);
+        resultDiscussion.setData(discussionListMap);
+        resultDiscussion.setCode(200);
+        resultDiscussion.setMessage("success");
+        return resultDiscussion;
     }
 
-    // 添加新的讨论区内容，首先将新的讨论放到数据库中
-    @PostMapping(path = "addNewDiscussion", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void submitDiscussion(@ModelAttribute DiscussionRequest discussionRequest){
-        // 用户即当前用户
+
+    // 添加新的讨论
+    @RequestMapping(value = "addNewDiscussion",method = RequestMethod.POST)
+    public Result<Integer> submitDiscussion1(@RequestParam("parentId") int parentId,
+                                             @RequestParam("topicId") int topicId,@RequestParam("content") String content){
         Users user = UserContext.getCurrentUser();
-        discussionService.addNewDiscussion(discussionRequest,user.getId());
+        discussionService.addNewDiscussion(user.getId(),parentId,topicId,content);
+        Result<Integer> resultNewDiscussion = new Result<>();
+        resultNewDiscussion.setMessage("success");
+        resultNewDiscussion.setCode(200);
+        resultNewDiscussion.setData(null);
+        resultNewDiscussion.setSuccess(true);
+        return resultNewDiscussion;
     }
-
     // 点赞
-    @PostMapping(path = "giveOneLike", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void updataLikeNum(@ModelAttribute DiscussionRequest discussionRequest){
-        discussionService.giveOneLike(discussionRequest);
-    }
-
-    @PostMapping(path = "giveOneLike1", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void updataLikeNum1(@RequestParam("discussionId") int discussionId){
-        discussionService.giveOneLike1(discussionId);
+    @RequestMapping(value = "giveOneLike", method = RequestMethod.POST)
+    public Result<Integer> updataLikeNum(@RequestParam("discussionId") int discussionId){
+        discussionService.giveOneLike(discussionId);
+        Result<Integer> resultLikeNum = new Result<>();
+        resultLikeNum.setSuccess(true);
+        resultLikeNum.setCode(200);
+        resultLikeNum.setMessage("success");
+        resultLikeNum.setData(null);
+        return resultLikeNum;
     }
 }
